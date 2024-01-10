0b2cb298a996528c7d6803ab7a5a43658e91c191
Valentin <deathman777@gmail.com>
2024-01-10 23:21:18 +0500

    added postgres sql realisation of user service

--- server/src/main/java/ru/gordeev/december/chat/ServerApplication.java
+++ server/src/main/java/ru/gordeev/december/chat/ServerApplication.java
@@ -1,11 +1,5 @@
 package ru.gordeev.december.chat;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerApplication {
 
     public static void main(String[] args) {
